06cf3e1b949f3de64349b964ef11759608b98863
Illia Damaratski <illiadamaratski@gmail.com>
2024-03-25 23:16:25 +0300

    fill contacts page

--- src/components/page/contacts/Contacts.tsx
+++ src/components/page/contacts/Contacts.tsx
@@ -3,6 +3,14 @@ function Contacts() {
     return <div className="tab-content">
         <section className="tab-section">
             <p className="text-page-header">Контакты</p>
+            <p><b>Адрес</b></p>
+            <p>г. Минск, ул. Высокая, д. 64</p>
+            <br/>
+            <p><b>Телефон</b></p>
+            <p>+375 29 117 00 11</p>
+            <br/>
+            <p><b>Социальные сети</b></p>
+            <p>instagram: cvetok.by</p>
         </section>
     </div>
 }
